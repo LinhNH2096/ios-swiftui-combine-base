56d2cfd9f3ebf32be5b25c68ee173acd2097da29
Nguyen Hong Linh <linh.nh@dgwasia.com>
2024-08-12 11:37:37 +0700

    - Initial commit

---
+++ ios-swiftui-combine/ios_swiftui_combineApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftui_combineApp.swift
+//  ios-swiftui-combine
+//
+//  Created by LinhNguyen DGWA on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftui_combineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
